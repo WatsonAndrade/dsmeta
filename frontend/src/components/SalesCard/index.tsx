bc47ab8985bee3c9d4956ed61da9d67538d19dcc
Watson Andrade <watsonleidson@gmail.com>
2022-07-16 06:53:24 -0300

    Send notification

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -21,8 +21,6 @@ function SalesCard() {
     const dmin = minDate.toISOString().slice(0, 10);
     const dmax = maxDate.toISOString().slice(0, 10);
     
-    console.log(dmin);
-    
     axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`).then((response) => {
       setSales(response.data.content);
     });
@@ -63,7 +61,7 @@ function SalesCard() {
             </tr>
           </thead>
           <tbody>
-            {sales.map((sale) => {
+            {sales.map(sale => {
               return (
                 <tr key={sale.id}>
                   <td className="show992">{sale.id}</td>
@@ -74,7 +72,7 @@ function SalesCard() {
                   <td>R$ {sale.amount.toFixed(2)}</td>
                   <td>
                     <div className="dsmeta-red-btn-container">
-                      <NotificationButton />
+                      <NotificationButton saleId={sale.id} />
                     </div>
                   </td>
                 </tr>
